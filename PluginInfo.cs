7566ee87ac3284c37eb9f67dc4834c7c999f0b19
yourboypog <156524181+yourboypog@users.noreply.github.com>
2026-03-25 22:28:01 -0400

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    public class PluginInfo
+    {
+        public const string GUID = "corp.cylvia";
+        public const string Name = "cylvia.";
+        public const string Description = "a free and undetectable cheat client.";
+        public const string Version = "1.0.0";
+    }
+}
